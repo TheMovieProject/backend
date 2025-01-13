61eb402745339956f904911ad5921c465f9f545b
Aditya30december2003 <adityasmjain@gmail.com>
2025-01-14 00:55:17 +0530

    made changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { getAuthSession } from "@/app/api/auth/[...nextauth]/options";
 import HomePage from '@/app/(pages)/Home/Home'
 import { redirect } from "next/navigation";
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  // if (!session) {
-  //   redirect("/login");
-  // }
+   const session = await getAuthSession()
+  if (!session) {
+    redirect("/login");
+  }
 
   return (
     <main className="">
