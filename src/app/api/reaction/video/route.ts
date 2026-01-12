e1693e9424a7f3aa867a5e9b3a9ab2c7071219e2
Aditya30december2003 <adityasmjain@gmail.com>
2026-01-12 10:01:46 +0530

    debugged route reactions

--- src/app/api/reaction/video/route.ts
+++ src/app/api/reaction/video/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismaDB";
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 
 
 export async function POST(req: Request) {
-const session = await getServerSession(authOptions as any);
+const session = await getServerSession(authOptions);
 if (!session?.user?.id) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 const { videoId } = await req.json();
 try {
